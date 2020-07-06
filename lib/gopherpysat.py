0f17841818cb40678e8a6848dd0a0933a508a950
baguette eater <ulysse.brehpn@gmail.com>
2020-07-07 01:56:48 +0200

    + count_model ; remove_model ; remove_pretty_model

--- lib/gopherpysat.py
+++ lib/gopherpysat.py
@@ -9,14 +9,25 @@ from typing import List, Dict
 __author__ = "Sylvain Lagrue"
 __copyright__ = "Copyright 2019, 2020 UTC"
 __license__ = "LGPL-3.0"
-__version__ = "0.4.0"
-__maintainer__ = "Sylvain Lagrue"
-__email__ = "sylvain.lagrue@utc.fr"
+__version__ = "0.4.1"
+__maintainer__ = "Ulysse Brehon"
+__email__ = "ulysse.brehon@gmail.com"
 __status__ = "dev"
 
 USAGE = f"""usage: python {os.sys.argv[0]} cnf_file"""
 
 """
+ADDED IN 0.4.1
+-> count_model()
+Thanks to the API of the Gophersat exec, we use the '-count' option to count the number of pssible models
+Can take a while, use with caution
+
+-> remove_clause(self, clause)
+Allow the user to remove a specific clause in their gophersat format
+
+-> remove_pretty_clause(self, pretty_clause)
+Allow the user to pop a specific clause from the clause base in the pretty format
+
 TODO :
 - vérif qu'on n'a pas ajouté une variable de plus au moment d'ajouter les clauses
 - vérif que la liste de voc est de taille suffisante...
@@ -31,6 +42,7 @@ class Gophersat:
         # self.find_exec()
         self.__executable = gophersat_exec
 
+        self.__nb_model = 0
         self.__voc = voc
         self.__has_changed = False
         self.__satisfiable = True
@@ -47,6 +59,7 @@ class Gophersat:
             self.voc2dictionnary()
 
     def changed(self):
+        self.__nb_model = 0
         self.__has_changed = True
         self.__satisfiable = None
         self.__model = []
@@ -80,6 +93,21 @@ class Gophersat:
         self.__clauses = self.__clauses[:-1]
         self.changed()
 
+    def remove_clause(self, clause):
+        self.__clauses.remove(clause)
+        self.changed()
+
+    def remove_pretty_clause(self, pretty_clause: List[str]):
+        l = []
+
+        for lit in pretty_clause:
+            if lit[0] == "-" or lit[0] == "¬":
+                l.append(-(self.__voc_dict[lit[1:]] + 1))
+            else:
+                l.append((self.__voc_dict[lit] + 1))
+
+        self.remove_clause(l)
+
     def solve(self):
         if not (self.__has_changed):
             return self.__satisfiable
@@ -93,7 +121,7 @@ class Gophersat:
         res = subprocess.run(
             [self.__executable, f.name], stdout=subprocess.PIPE, stderr=subprocess.PIPE
         )
-
+        
         os.remove(temporary_file_name)
 
         err = res.stderr.decode("utf8")
@@ -114,6 +142,34 @@ class Gophersat:
             self.__satisfiable = False
 
         return self.__satisfiable
+
+    def count_model(self):
+        if not (self.__has_changed):
+            return self.__satisfiable
+
+        temporary_dir = tempfile.gettempdir()
+        temporary_file_name = f"{os.path.join(temporary_dir, uuid.uuid4().hex)}.cnf"
+
+        with open(temporary_file_name, "w", newline="\n") as f:
+            f.write(self.dimacs())
+
+        res = subprocess.run(
+            [self.__executable, "-count",  f.name], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+
+        os.remove(temporary_file_name)
+
+        err = res.stderr.decode("utf8")
+        if err != "":
+            print("error:", err)
+            sys.exit(2)
+
+        out = res.stdout.decode("utf8")
+        lines = out.splitlines()
+
+        self.__nb_model = int(lines[1])
+
+        return self.__nb_model
 
     def get_model(self):
         return self.__model
